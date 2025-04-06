ecf547d216bdac89878cb237e80cc6497c076e6e
Jahid Hasan <jahidhasanbabu7821@gmail.com>
2025-04-06 23:19:37 +0600

    if else done

---
+++ module2/if_else.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int tk ;
+    scanf("%d", &tk);
+    if(tk >= 100){
+        printf("Burger khabo\n");
+    }else{
+        printf("Burger khabo na \n");
+    }
+    return 0;
+}
