f2df48b438494aba5b22a24d564cafc795196825
Jahid Hasan <jahidhasanbabu7821@gmail.com>
2025-04-07 22:56:47 +0600

    double

--- module2/if_else_ladder.c
+++ module2/if_else_ladder.c
@@ -1,13 +1,21 @@
+// #include <stdio.h>
+// int main(){
+//     int tk;
+//     scanf("%d", &tk);
+//     if(tk >= 100){
+//         printf("Burger khabo");
+//     }else if(tk >= 50){
+//         printf("Fuchka khabo");
+//     }else{
+//         printf("Not eating");
+//     }
+//     return 0;
+// }
+
+
+
 #include <stdio.h>
 int main(){
-    int tk;
-    scanf("%d", &tk);
-    if(tk >= 100){
-        printf("Burger khabo");
-    }else if(tk >= 50){
-        printf("Fuchka khabo");
-    }else{
-        printf("Not eating");
-    }
-    return 0;
+    double a = 15 / 4;
+    printf("%lf", a);
 }
