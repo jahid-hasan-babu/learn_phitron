be47dfae35082f2e56f2eab6096605bd26325f7e
Jahid Hasan <jahidhasanbabu7821@gmail.com>
2025-04-07 22:38:38 +0600

    if else ladder

---
+++ module2/if_else_ladder.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int tk;
+    scanf("%d", &tk);
+    if(tk >= 100){
+        printf("Burger khabo");
+    }else if(tk >= 50){
+        printf("Fuchka khabo");
+    }else{
+        printf("Not eating");
+    }
+    return 0;
+}
