420147307da491a3206d7191555e530dcf5aced8
Jahid Hasan <jahidhasanbabu7821@gmail.com>
2025-04-15 19:55:39 +0600

    print hello

--- module4/max_and_min.c
+++ module4/max_and_min.c
@@ -1,23 +1,35 @@
+// #include<stdio.h>
+
+// int main(){
+//     int a, b, c;
+//     scanf("%d %d %d", &a, &b, &c);
+//     //minimum
+//     if(a <= b && a <= c){
+//         printf("%d ", a);
+//     }else if(b <= a && b <= c){
+//         printf("%d ", b);
+//     }else if(c <= a && c <= b){
+//         printf("%d ", c);
+//     }
+//     //maximum
+//     if(a >= b && a >= c){
+//         printf("%d", a);
+//     }else if(b >= a && b >= c){
+//         printf("%d", b);
+//     }else if(c >= a && c >= b){
+//         printf("%d", c);
+//     }
+//     return 0;
+// }
+
 #include<stdio.h>
 
 int main(){
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
-    //minimum
-    if(a <= b && a <= c){
-        printf("%d ", a);
-    }else if(b <= a && b <= c){
-        printf("%d ", b);
-    }else if(c <= a && c <= b){
-        printf("%d ", c);
+    int x = 5;
+    int y = 4;
+    if(x >=y && x<y+4 ){
+        printf("hello");
     }
-    //maximum
-    if(a >= b && a >= c){
-        printf("%d", a);
-    }else if(b >= a && b >= c){
-        printf("%d", b);
-    }else if(c >= a && c >= b){
-        printf("%d", c);
-    }
+     
     return 0;
 }
