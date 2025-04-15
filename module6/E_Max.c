f0f597c43f04528e107dae1b07856942cd42e8e8
Jahid Hasan <jahidhasanbabu7821@gmail.com>
2025-04-15 21:06:47 +0600

    negative minimum

--- module6/E_Max.c
+++ module6/E_Max.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int mx = 0;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         int val;
+//         scanf("%d", &val);
+//         if (val > mx)
+//         {
+//             {
+//                 mx = val;
+//             }
+//         }
+//     }
+//     printf("%d", mx);
+//     return 0;
+// }
+#include <limits.h>
+
 int main()
 {
     int n;
     scanf("%d", &n);
-    int mx = 0;
+    int mx = INT_MIN;
     for (int i = 1; i <= n; i++)
     {
         int val;
