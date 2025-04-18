10cbd18c0cc033724ee6f8e5fa43c67a5b2fad07
Jahid Hasan <jahidhasanbabu7821@gmail.com>
2025-04-18 20:33:04 +0600

    nested loop

--- module6/nested_loop.c
+++ module6/nested_loop.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    for(int i = 1; i <= 5; i++){
-        for(int j= 1; j <= 5;j++){
-            printf("%d", j);
+int main()
+{
+
+    for (int i = -4; i >= 0; i++)
+    {
+        for (int j = 5; j < 2; j++)
+        {
+            printf("hi");
         }
-        printf("\n");
     }
+
     return 0;
 }
