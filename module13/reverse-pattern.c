f44c3caa944dbae583291142532217593b182799
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2025-05-05 21:41:24 +0600

    reverse pemarid done

--- module13/reverse-pattern.c
+++ module13/reverse-pattern.c
@@ -3,17 +3,25 @@
 int main(){
     int n;
     scanf("%d", &n);
-    int star = n;
+    int star = 2 * n - 1; 
+    int space = 0;
+
     for (int i = 1; i <= n; i++)
     {
-       for (int  j = 1; j <= star; j++)
-       {
-        printf("*");
-       }
-       printf("\n");
-       star--;
-       
+        for (int k = 1; k <= space; k++)
+        {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= star; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+        space++;
+        star -= 2;
     }
-    
+
     return 0;
 }
--- module13/reverse-pemarid.c
+++ module13/reverse-pemarid.c
@@ -4,21 +4,21 @@ int main(){
     int n;
     scanf("%d", &n);
     int star = n;
-    int space = n - 1;
+    int space = n- 1;
     for (int i = 1; i <= n; i++)
     {
-        for (int  k = n; k <= space; k++)
+        for (int  k = 1; k <= space; k++)
         {
             printf(" ");
         }
         
-        for (int j = 0; j <=  star; j++)
+        for (int j = 1; j <=  star; j++)
         {
             printf("*");
         }
     
        printf("\n");
-       star-=2;
+       star--;
        space++;
     }
 
