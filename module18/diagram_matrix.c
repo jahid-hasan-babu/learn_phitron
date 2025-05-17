951e48cafd4db5e64bfb3cbf72d17fd0e0cd8d2c
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2025-05-17 21:32:39 +0600

    diagonal matrix done 2

--- module18/diagram_matrix.c
+++ module18/diagram_matrix.c
@@ -1,3 +1,45 @@
+// #include<stdio.h>
+// #include<stdbool.h>
+
+// int main(){
+//     int r, c;
+//     scanf("%d %d", &r, &c);
+//     int a[r][c];
+//     for (int i = 0; i < r; i++)
+//     {
+//        for (int j = 0; j < c; j++)
+//        {
+//         scanf("%d", &a[i][j]);
+//        }
+       
+//     }
+//     bool is_diagonal = true;
+   
+//     if(r ==c ){ //square matrix
+//         for (int i = 0; i < r; i++)
+//         {
+//            for (int j = 0; j < c; j++)
+//            {
+//             if(i == j){
+//                 // printf("%d ", a[i][j]);
+//             }else{
+//                 if(a[i][j] != 0){
+//                     is_diagonal = false;
+//                     printf("This is not a diagonal matrix");
+//                 }
+//             }
+//            }
+           
+//         }
+//         if(is_diagonal == true){
+//             printf("This is diagonal");
+//         }
+//     }
+    
+//     return 0;
+// }
+
+
 #include<stdio.h>
 #include<stdbool.h>
 
@@ -20,14 +62,12 @@ int main(){
         {
            for (int j = 0; j < c; j++)
            {
-            if(i == j){
-                // printf("%d ", a[i][j]);
-            }else{
+            if(i != j){
                 if(a[i][j] != 0){
                     is_diagonal = false;
                     printf("This is not a diagonal matrix");
                 }
-            }
+                }
            }
            
         }
